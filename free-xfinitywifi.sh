b2c8e2c305218584f8598b45a57c914511deb1c5
sys-3 <sys3@ctemplar.com>
2020-07-24 17:07:33 -0700

    Updated the script with some better wording, and now sends a notification with the notify-send command instead of just echoing in the terminal.

--- free-xfinitywifi.sh
+++ free-xfinitywifi.sh
@@ -2,11 +2,11 @@
 #This script requires your DNS to be 75.75.75.75 so xfinitywifi can redirect you to the correct pages, also requires macchanger to be installed.
 
 WIFIID=wlo1 #Enter your wifi ID here ("ip link show")
-BROWSER=brave-dev #Enter your browser here
+BROWSER=brave-dev #Enter your preferred browser browser here
 sudo ip link set dev $WIFIID down
 sudo macchanger -r $WIFIID
 sudo ip link set dev $WIFIID up
 sudo killall NetworkManager
 sudo systemctl restart NetworkManager
 $BROWSER 75.75.75.75
-echo "Enter login details and choose your hour pass in your browser!"
+echo "Enter your login details and choose your hour pass in your browser!" ; notify-send "Enter your login details and choose your hour pass in your browser!"
