6d22fe5072827a2e969b98d98bde25f2ba222a74
sys-3 <sys3@ctemplar.com>
2021-08-08 16:28:34 -0700

    Removed instances of sudo command, improved comments

--- free-xfinitywifi.sh
+++ free-xfinitywifi.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#This script requires your DNS to be 75.75.75.75 so xfinitywifi can redirect you to the correct pages, also requires macchanger to be installed.
+#This script requires your DNS to be 75.75.75.75, because it's only the only whitelisted DNS server,
+#this script also requires macchanger to be installed,
+#and lastly, this script requires root permissions.
 
-WIFIID=wlo1 #Enter your wifi ID here ("ip link show")
-BROWSER=brave-dev #Enter your preferred browser browser here
-sudo ip link set dev $WIFIID down
-sudo macchanger -r $WIFIID
-sudo ip link set dev $WIFIID up
-sudo killall NetworkManager
-sudo systemctl restart NetworkManager
+WIFI-ID=wlo1 #Enter your wifi card's ID here ("ip link show")
+BROWSER=firefox #Enter your preferred browser browser here
+ip link set dev $WIFI-ID down
+macchanger -r $WIFI-ID
+ip link set dev $WIFI-ID up
+systemctl restart NetworkManager
 $BROWSER 75.75.75.75
 echo "Enter your login details and choose your hour pass in your browser!" ; notify-send "Enter your login details and choose your hour pass in your browser!"
